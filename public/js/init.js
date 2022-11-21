f2304eeabb71cfd8202f3ee2377443615345b008
Viki <hi@viki.moe>
2022-11-21 23:31:02 +0800

    chore: remove public

--- public/js/init.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('DOMContentLoaded', function () {
-  var cr = document.getElementById('copyright')
-  cr.innerText = cr.innerText.replace('[year]', new Date().getFullYear())
-})
