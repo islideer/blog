70cb2843f04b0344aa1996b409c5790dc2a1f83f
Viki <hi@viki.moe>
2022-11-22 09:21:05 +0800

    feat: 调整 UI

--- themes/mio/source/js/init.js
+++ themes/mio/source/js/init.js
@@ -2,3 +2,5 @@ document.addEventListener('DOMContentLoaded', () => {
   // listen to any DOM change and automatically perform spacing via MutationObserver()
   pangu.autoSpacingPage()
 })
+
+const demo = 123;
