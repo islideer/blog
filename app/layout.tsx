df7e695e5f57fd7393ffb8cab05f332ff762f176
Viki <hi@viki.moe>
2025-11-28 12:22:16 +0800

    chore: 移除过渡

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,7 +96,7 @@ export default function RootLayout({
         />
       </head>
       <body className="bg-bg-primary text-text-primary font-sans antialiased duration-300">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <Analytics />
           <GoogleAnalytics gaId={siteConfig.analytics.google} />
           <div className="flex min-h-screen min-w-sm flex-col sm:mx-auto sm:max-w-3xl">
