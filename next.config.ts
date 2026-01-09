940678f7ef243eeaf081bcb8fbec6f00f6903a4c
Viki <hi@viki.moe>
2026-01-09 15:55:33 +0800

    chore: 禁用图片优化

--- next.config.ts
+++ next.config.ts
@@ -11,17 +11,18 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   reactCompiler: true,
   images: {
-    remotePatterns: [
-      { hostname: '**.loli.net' },
-      { hostname: '**.viki.moe' },
+    // remotePatterns: [
+    //   { hostname: '**.loli.net' },
+    //   { hostname: '**.viki.moe' },
 
-      // 友链头像部分
-      { hostname: 'www.shiro.team' },
-      { hostname: 'thricecola.com' },
-      { hostname: 'yunnet.top' },
-      { hostname: 'www.lapis.cafe' },
-      { hostname: 'blog.dogxi.me' },
-    ],
+    //   // 友链头像部分
+    //   { hostname: 'www.shiro.team' },
+    //   { hostname: 'thricecola.com' },
+    //   { hostname: 'yunnet.top' },
+    //   { hostname: 'www.lapis.cafe' },
+    //   { hostname: 'blog.dogxi.me' },
+    // ],
+    unoptimized: true,
   },
 }
 
