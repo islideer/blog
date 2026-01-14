0ea371d115ee949452581647307e24e78e2c98c5
Viki <hi@viki.moe>
2026-01-14 15:06:59 +0800

    chore: 三栏布局

--- components/steam-game-library.tsx
+++ components/steam-game-library.tsx
@@ -167,7 +167,7 @@ function GamesList({
 }) {
   return (
     <ImageZoomProvider deps={[games]}>
-      <div className="grid grid-cols-2 gap-4 sm:grid-cols-4">
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
         {games.map((game) => (
           <div
             key={game.appid}
