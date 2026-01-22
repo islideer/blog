e9f98cd75b10e6c419a768c29286d2c16a4f3186
Viki <hi@viki.moe>
2026-01-22 12:28:06 +0800

    Update interaction-button.tsx

--- components/interaction-button.tsx
+++ components/interaction-button.tsx
@@ -176,7 +176,7 @@ export function InteractionButton({ id, type, className }: InteractionButtonProp
           <IconComponent
             className={cn('h-[1.2em] w-[1.2em] transition-transform', isMaxedOut && 'scale-110')}
           />
-          ｛displayCount > 0 && (<span className="font-mono tabular-nums">
+          {displayCount > 0 && (<span className="font-mono tabular-nums">
             {displayCount.toLocaleString('zh-Hans-CN')}
           </span>)}
         </button>
