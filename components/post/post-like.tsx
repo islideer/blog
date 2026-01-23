058b52c1449ac996ded315314fbb95bc776a09b2
Viki <hi@viki.moe>
2026-01-23 18:08:32 +0800

    fix: 优化 UI

--- components/post/post-like.tsx
+++ components/post/post-like.tsx
@@ -7,22 +7,24 @@ interface PostLikeProps {
 }
 
 export function PostLike({ slug }: PostLikeProps) {
+  const icon = (
+    <InteractionButton
+      id={slug}
+      type="posts"
+      className="flex-col justify-start! gap-1 text-lg opacity-48 hover:opacity-100 sm:text-xl"
+      iconClassName="text-2xl sm:text-3xl"
+    />
+  )
+
   return (
     <>
       {/* PC 端 - 固定在左侧，与目录对称 */}
       <div className="fixed top-1/2 left-4 z-10 hidden min-h-64 w-60 -translate-y-1/2 text-right transition-all xl:block">
-        <InteractionButton
-          id={slug}
-          type="posts"
-          className="flex-col justify-start! gap-1 text-lg opacity-48 hover:opacity-100 sm:text-xl"
-          iconClassName="text-2xl sm:text-3xl"
-        />
+        {icon}
       </div>
 
       {/* 移动端 - 文章底部居中 */}
-      <div className="flex items-center justify-center xl:hidden">
-        <InteractionButton id={slug} type="posts" className="gap-2 text-xl sm:text-2xl" />
-      </div>
+      <div className="flex items-center justify-center xl:hidden">{icon}</div>
     </>
   )
 }
