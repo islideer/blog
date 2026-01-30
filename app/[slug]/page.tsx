92f08c5b67ca52a013bc80674a8cf7ae36e95217
Viki <hi@viki.moe>
2026-01-30 16:02:39 +0800

    chore: 优化细节

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -154,16 +154,6 @@ export default async function BlogPostPage({ params }: { params: Promise<{ slug:
         {/* Article End */}
         <div className="border-border mt-8 border-t pt-8 sm:mt-16">
           <p className="text-text-tertiary mb-4 text-center text-sm">—— 本文完 ——</p>
-          <p className="text-text-tertiary mx-auto flex justify-center gap-2 text-center text-sm">
-            <ChatIconIcon className="h-6 w-6" />
-            <span>
-              交流讨论、反馈建议，请到
-              <Link href="/messages" className="mx-1">
-                话匣子
-              </Link>
-              页面留言。
-            </span>
-          </p>
         </div>
 
         {/* Post Like */}
@@ -175,6 +165,19 @@ export default async function BlogPostPage({ params }: { params: Promise<{ slug:
           <div className="flex flex-1 truncate px-2 sm:justify-center">
             <PostInfo title={post.title} slug={post.slug} />
           </div>
+        </div>
+
+        <div className="border-border mt-8 border-t pt-8 sm:mt-16">
+          <p className="text-text-tertiary mx-auto flex justify-center gap-2 text-center text-sm">
+            <ChatIconIcon className="h-5 w-5" />
+            <span>
+              交流讨论、反馈建议，请到
+              <Link href="/messages" className="mx-1">
+                话匣子
+              </Link>
+              页面留言。
+            </span>
+          </p>
         </div>
 
         {/* Recommended Posts */}
